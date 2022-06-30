16c526b84d055aa647688b29791a9d14f2a4e896
coinop-logan <coinop.logan@gmail.com>
2022-06-30 10:59:34 -0800

    Config file and fetch all

---
+++ fetchall.py
@@ -0,0 +1,29 @@
+import requests, json
+from pprint import pprint
+
+def loadKey():
+    with open('key','r') as f:
+        return f.read()
+
+def loadConfig():
+    with open('config.json','r') as f:
+        return json.load(f)
+
+def fetchPrice(priceIdentifier):
+    key = loadKey()
+    url = "https://api.cryptowat.ch/markets/kraken/" + priceIdentifier + '/price?apikey=' + key
+
+    response = requests.get(url)
+    return response.json()['result']['price']
+
+def main():
+    config = loadConfig()
+
+    fetchedPrices = {}
+
+    for priceIdentifier in config['priceIdentifiers']:
+        fetchedPrices[priceIdentifier] = fetchPrice(priceIdentifier)
+    
+    print(json.dumps(fetchedPrices))
+
+main()
